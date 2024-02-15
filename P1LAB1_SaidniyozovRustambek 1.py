88894c0a0448e9eeaf9a8496a5ebfe36383d8701
saidniyr1943 <160173186+saidniyr1943@users.noreply.github.com>
2024-02-15 14:36:40 -0500

    Add files via upload

---
+++ P1LAB1_SaidniyozovRustambek 1.py
@@ -0,0 +1,10 @@
+# Rustambek Saidniyozov
+# February 15 2024
+# PILAB1
+# This programm will display my name and a welcome message
+
+#get the first name from the user and assign it to a variable
+
+fname=input("Enter your first name: ")
+lname=input("Enter your last name: ")
+print("Hello,",fname,lname + "! Welcome to CTI-110.")
