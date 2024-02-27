8cc4ca9d2677e16ab3a7d842d44a440516be18a6
saidniyr1943 <160173186+saidniyr1943@users.noreply.github.com>
2024-02-27 14:07:51 -0500

    Add files via upload

---
+++ P2LAB1_SaidniyozovRustambek.py
@@ -0,0 +1,13 @@
+# Rustambek Saidniyozov
+# 2/20/2024
+# P2LAB1
+# Circle Formulas Calculator
+
+import math
+r = float(input('What is the radius of the circle?:\n'))
+d = r*2
+print(f"The diameter of the circle is {d:.1f}.")
+c=2*3.14159265*r
+print(f"The circumference of the circle is {c:.2f}.")
+a=3.14159265*math.pow(r,2)
+print(f"The area of the circle is {a:.3f}.")
